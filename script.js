28d33bef51347ea69ba8c17d42165464df5eefb3
ThyoDerive <derivetheo96@gmail.com>
2022-12-19 14:30:49 +0100

    1 vs 1 Check

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const ButtonPlayer = document.querySelectorAll('.Button')
 const PcChoice = document.querySelector('.PcChoice')
 let PCScore = 0
 let PlayerScore = 0
+let PlayersInput = []
+let Timer = 0
 
 document.querySelector('.contrePC').addEventListener('click', () => {
 
@@ -55,8 +57,60 @@ document.querySelector('.contrePC').addEventListener('click', () => {
 
         })
     })
+})
 
-    document.querySelector('.Replay').addEventListener('click', (e) => {
-        window.location.reload()
+document.querySelector('.Replay').addEventListener('click', (e) => {
+    window.location.reload()
+})
+
+
+document.querySelector('.contre1').addEventListener('click', () => {
+
+    document.querySelector('.ChoixMode').style.display = 'none'
+
+    
+
+    ButtonPlayer.forEach((i) => {
+        i.addEventListener('click', () => {
+            
+            PlayersInput.push(parseInt(i.id))
+
+            if (PCScore == 3 || PlayerScore == 3) {
+
+                document.querySelector('.Vainqueur').classList.add('VainqueurActive')
+
+                if (PCScore > PlayerScore) {
+                    document.querySelector('.ResultatFinal').innerHTML = 'Gagnez'
+                    document.querySelector('.PhraseFinal').innerHTML = 'Le Player 2 à gagnez'
+                }else {
+                    console.log('Le player a gagnez');
+                    document.querySelector('.ResultatFinal').innerHTML = 'Gagnez'
+                    document.querySelector('.PhraseFinal').innerHTML = 'Le Player 1 à gagnez'
+                }
+
+            }else{
+                if ((PlayersInput.length % 2) == 0) {
+                    
+                    if(PlayersInput[Timer] == PlayersInput[Timer + 1]){
+                        Result.innerHTML = "Egalite"
+                    }
+                    else if (PlayersInput[Timer + 1] == 3 && PlayersInput[Timer] == 2 || PlayersInput[Timer + 1]  == 2 && PlayersInput[Timer]  == 1 || PlayersInput[Timer + 1]  == 1 && PlayersInput[Timer] == 3){
+                        Result.innerHTML = 'Le player 2 le remporte'
+                        PCScore ++
+                        ScorePC.innerHTML = PCScore
+                    }
+                    else if (PlayersInput[Timer + 1]  == 3 && PlayersInput[Timer]  == 1 || PlayersInput[Timer + 1]  == 1 && PlayersInput[Timer]  == 2 || PlayersInput[Timer + 1]  == 2 && PlayersInput[Timer]  == 3){
+                        Result.innerHTML = 'Le player 1 le remporte'
+                        PlayerScore ++
+                        ScorePlayer.innerHTML = PlayerScore
+                    }
+
+                    Timer = Timer + 2
+
+                    console.log(Timer);
+                }
+            }
+
+        })
     })
 })
